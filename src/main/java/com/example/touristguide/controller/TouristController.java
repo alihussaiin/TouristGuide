908f0f13f0975f682ed2b0d16ab6976e9cd6c303
alihussaiin <alhu0002@stud.kea.dk>
2024-02-23 12:59:54 +0100

    arbejdet lidt forskelligt

--- src/main/java/com/example/touristguide/controller/TouristController.java
+++ src/main/java/com/example/touristguide/controller/TouristController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class TouristController {
 
 
-    private  final TouristService touristService;
+    private final TouristService touristService;
 
     public TouristController(TouristService touristService) {
 
@@ -82,9 +82,9 @@ public class TouristController {
         TouristAttraction attraction = touristService.getAttractionByName(name);
         if (attraction != null) {
             model.addAttribute("description", attraction.getDescription());
-            return "attractionDescription"; // Returnerer navnet på Thymeleaf-skabelonen
+            return "attractionDescription";
         } else {
-            return "attractionNotFound"; // Returnerer navnet på Thymeleaf-skabelonen
+            return "attractionNotFound";
         }
     }
 
@@ -93,9 +93,9 @@ public class TouristController {
         List<String> tags = touristService.getAttractionTagsByName(name);
         if (!tags.isEmpty()) {
             model.addAttribute("tags", tags);
-            return "tagsList"; // Returnerer navnet på Thymeleaf-skabelonen
+            return "tagsList";
         } else {
-            return "tagsNotFound"; // Returnerer navnet på Thymeleaf-skabelonen
+            return "tagsNotFound";
         }
     }
 
@@ -103,94 +103,8 @@ public class TouristController {
     public String getEditAttractionPage(@PathVariable String name, Model model) {
         String editPageContent = touristService.getEditAttractionPage(name);
         model.addAttribute("editContent", editPageContent);
-        return "editPage"; // Returnerer navnet på Thymeleaf-skabelonen
+        return "editPage";
     }
-
-
-    /*@PostMapping("/add")
-    public ResponseEntity<TouristAttraction> createAttraction(@RequestBody TouristAttraction attraction) {
-        TouristAttraction createdAttraction = touristService.createAttraction(attraction);
-        return new ResponseEntity<>(createdAttraction, HttpStatus.CREATED);
-    }
-
-
-    @GetMapping("/description/{description}")
-    public ResponseEntity<TouristAttraction> getAttractionByDescription(@PathVariable String description) {
-        TouristAttraction attraction = touristService.getAttractionByDescription(description);
-        if (attraction != null) {
-            return new ResponseEntity<>(attraction, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/name/{name}")
-    public ResponseEntity<List<TouristAttraction>> getAttractionsByName(@PathVariable String name) {
-        List<TouristAttraction> attractions = touristService.getAttractionsByName(name);
-        if (!attractions.isEmpty()) {
-            return new ResponseEntity<>(attractions, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    // UPDATE endpoints
-    @PutMapping("/update/{description}")
-    public ResponseEntity<TouristAttraction> updateAttractionByDescription(@PathVariable String description, @RequestBody TouristAttraction attraction) {
-        TouristAttraction updatedAttraction = touristService.updateAttractionByDescription(description, attraction);
-        if (updatedAttraction != null) {
-            return new ResponseEntity<>(updatedAttraction, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<TouristAttraction> updateAttraction(@RequestBody TouristAttraction attraction) {
-        TouristAttraction updatedAttraction = touristService.updateAttraction(attraction);
-        if (updatedAttraction != null) {
-            return new ResponseEntity<>(updatedAttraction, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    // DELETE endpoints
-    @DeleteMapping("/delete/{description}")
-    public ResponseEntity<String> deleteAttractionByDescription(@PathVariable String description) {
-        boolean deleted = touristService.deleteAttractionByDescription(description);
-        if (deleted) {
-            return new ResponseEntity<>("Attraktion med beskrivelsen \"" + description + "\" er blevet slettet.", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Attraktion med beskrivelsen \"" + description + "\" blev ikke fundet.", HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/{name}")
-    public ResponseEntity<String> getAttractionDescriptionByName(@PathVariable String name) {
-        TouristAttraction attraction = touristService.getAttractionByName(name);
-        if (attraction != null) {
-            return new ResponseEntity<>(attraction.getDescription(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Attraktionen med navnet \"" + name + "\" blev ikke fundet.", HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/{name}/tags")
-    public ResponseEntity<List<String>> getAttractionTagsByName(@PathVariable String name) {
-        List<String> tags = touristService.getAttractionTagsByName(name);
-        if (tags != null) {
-            return new ResponseEntity<>(tags, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/{name}/edit")
-    public ResponseEntity<String> getEditAttractionPage(@PathVariable String name) {
-        // Tilføj logik for at hente og vise redigeringsformular
-        return new ResponseEntity<>("Edit page for " + name, HttpStatus.OK);
-    } */
 
 
 }
--- src/main/java/com/example/touristguide/model/TouristAttraction.java
+++ src/main/java/com/example/touristguide/model/TouristAttraction.java
@@ -7,33 +7,50 @@ public class TouristAttraction {
     private String description;
     private List<String> tags; // Ny attribut til tags
 
-    public TouristAttraction(String name, String description, List<String> tags) {
+    private String city;
+
+    public TouristAttraction(String name, String description, List<String> tags, String  city) {
         this.name = name;
         this.description = description;
         this.tags = tags;
+        this.city = city;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
     public String getName() {
+
         return name;
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public void setName(String name) {
+
         this.name = name;
     }
 
     public List<String> getTags() {
+
         return tags;
     }
 
     public void setTags(List<String> tags) {
+
         this.tags = tags;
     }
 }
